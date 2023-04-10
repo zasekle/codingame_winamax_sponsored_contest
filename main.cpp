8752200f87aa16d04639312fdc27acdeb1a2e617
Jeremiah Gleason <quickshiftx@gmail.com>
2023-04-09 22:24:39 -0400

    This is a working solution for the problem. However, it can be cleaned up by quite a lot, follow the TODOs in the code.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,14 @@ struct BallPathPoint {
             int _col
     ) : row(_row),
         col(_col) {}
+
+    ~BallPathPoint() = default;
+
+    BallPathPoint(const BallPathPoint &other) = default;
+
+    BallPathPoint(BallPathPoint &&other) = default;
+
+    BallPathPoint &operator=(const BallPathPoint &other) = default;
 };
 
 struct Ball {
@@ -68,6 +76,11 @@ enum LineIsValidReturn {
     LINE_VALID_NOT_IN_HOLE,
 };
 
+bool find_unique_solution(
+        std::vector<std::vector<std::vector<BallPathPoint>>> &passed_paths,
+        std::vector<std::vector<std::vector<BallPathPoint>>> &return_value
+);
+
 LineIsValidReturn check_if_line_is_valid(
         int number_moves,
         std::vector<std::string> &course,
@@ -95,15 +108,20 @@ bool find_path_for_next_ball(
 int main() {
 
     //#3
-    std::vector<std::string> course{
-            "4..XX",
-            ".H.H.",
-            "...H.",
-            ".2..2",
-            "....."
-    };
+//    std::vector<std::string> course{
+//            "4..XX",
+//            ".H.H.",
+//            "...H.",
+//            ".2..2",
+//            "....."
+//    };
 
     //#4
+    // ball_loc[0].paths[1]
+    // ball_loc[1].paths[0]
+    // ball_loc[2].paths[1]
+    // ball_loc[3].paths[2]
+    // ball_loc[4].paths[0]
 //    std::vector<std::string> course{
 //            "3..H.2",
 //            ".2..H.",
@@ -114,16 +132,16 @@ int main() {
 //    };
 
     //#5
-//    std::vector<std::string> course{
-//            ".XXX.5X.",
-//            "X.4.X..X",
-//            "X4..X3.X",
-//            "X...X.X.",
-//            ".X.X.H.X",
-//            "X.HX...X",
-//            "X..X.H.X",
-//            ".XH.XXX."
-//    };
+    std::vector<std::string> course{
+            ".XXX.5X.",
+            "X.4.X..X",
+            "X4..X3.X",
+            "X...X.X.",
+            ".X.X.H.X",
+            "X.HX...X",
+            "X..X.H.X",
+            ".XH.XXX."
+    };
 
     //#9
 //    std::vector<std::string> course{
@@ -137,12 +155,16 @@ int main() {
 //            ".XHXXXXX..H3H...H2X.H..3X2..HXX3H.2XXXXH"
 //    };
 
+    //TODO: this is a working solution for the problem
+    //TODO: need to go over this and optimize it and clean the code a little
+    //TODO: do the Feynman technique after I finish and make sure I can explain it at the level of a child
+
     std::vector<Ball> ball_locs;
 
     for (int row = 0; row < course.size(); ++row) {
         for (int col = 0; col < course[row].size(); ++col) {
             if (std::isdigit(course[row][col])) {
-                ball_locs.emplace_back(Ball{row, col});
+                ball_locs.emplace_back(row, col);
             }
         }
     }
@@ -169,28 +191,159 @@ int main() {
             std::cout << '\n';
         }
 
-        possibilities *= ball_locs[i].paths.size();
+        possibilities *= (int) ball_locs[i].paths.size();
     }
 
-    //TODO: too many possibilities 1,875,116,0032
-    //TODO: so I can do something like take path 1, exclude all other paths that overlap with it, then try all other possibilities?
-    // or more like go to the next path in the list and do the same, then if a ball has zero paths, move to the next possible outcome
-    std::cout << "possibilities: " << possibilities << '\n';
-//    for (std::string &s: course) {
-//        for (char &c: s) {
-//            if (c != '<'
-//                && c != '>'
-//                && c != 'v'
-//                && c != '^'
-//                    ) {
-//                c = '.';
-//            }
-//        }
-//    }
+    std::vector<std::vector<std::vector<BallPathPoint>>> paths_copy;
+    paths_copy.reserve(ball_locs.size());
+    for (const auto &paths: ball_locs) {
+        paths_copy.emplace_back(paths.paths);
+    }
+
+    std::vector<std::vector<std::vector<BallPathPoint>>> final_paths;
+    bool success = find_unique_solution(
+            paths_copy,
+            final_paths
+    );
+    std::reverse(final_paths.begin(), final_paths.end());
+
+    std::cout << "\nsuccess: " << success << std::endl;
+    std::cout << "final path\n";
+    for (int i = 0; i < final_paths.size(); i++) {
+        std::cout << "index: " << i << '\n';
+        for (const auto &path: final_paths[i]) {
+            for (const BallPathPoint &c: path) {
+                std::cout << "(" << c.row << ", " << c.col << ") ";
+            }
+            std::cout << '\n';
+        }
+    }
+
+    std::vector<std::string> final_course(course.size(), std::string(course[0].size(), '.'));
+
+    for (const auto &path_list: final_paths) {
+        const std::vector<BallPathPoint> &single_path = path_list[0];
+
+        BallPathPoint prev_point = single_path[0];
+
+        for (int i = 1; i < single_path.size(); ++i) {
+
+            if (prev_point.row == single_path[i].row) { //horizontal
+                if (prev_point.col < single_path[i].col) { //right
+                    for(int j = prev_point.col; j <= single_path[i].col; ++j) {
+                        final_course[prev_point.row][j] = '>';
+                    }
+                } else { //left
+                    for(int j = single_path[i].col; j <= prev_point.col; ++j) {
+                        final_course[prev_point.row][j] = '<';
+                    }
+                }
+            } else { //vertical
+                if (prev_point.row < single_path[i].row) { //down
+                    for(int j = prev_point.row; j <= single_path[i].row; ++j) {
+                        final_course[j][prev_point.col] = 'v';
+                    }
+                } else { //up
+                    for(int j = single_path[i].row; j <= prev_point.row; ++j) {
+                        final_course[j][prev_point.col] = '^';
+                    }
+                }
+            }
+            prev_point = single_path[i];
+        }
+
+        final_course[single_path.back().row][single_path.back().col] = '.';
+    }
+
+    for(const auto& s : final_course) {
+        std::cout << s << '\n';
+    }
+
+}
+
+bool find_unique_solution(
+        std::vector<std::vector<std::vector<BallPathPoint>>> &passed_paths,
+        std::vector<std::vector<std::vector<BallPathPoint>>> &return_value
+) {
+
+    const int current_paths_index = 5 - passed_paths.size();
+    std::cout << "paths index: " << current_paths_index << std::endl;
+    if (passed_paths.size() < 2) {
+        return_value.push_back(passed_paths[0]);
+        return true;
+    }
+    for (const std::vector<BallPathPoint> &initial_path: passed_paths[0]) {
+        std::vector<std::vector<std::vector<BallPathPoint>>> paths_copy;
+        std::cout << current_paths_index << " running" << std::endl;
+
+        for (int i = 1; i < passed_paths.size(); ++i) {
+            std::vector<std::vector<BallPathPoint>> valid_paths;
+            for (const std::vector<BallPathPoint> &ball_path: passed_paths[i]) {
+                BallPathPoint ball_prev_path_point = ball_path[0];
+                bool valid_path = true;
+                for (int j = 1; j < ball_path.size() && valid_path; ++j) {
+                    BallPathPoint initial_prev_path_point = initial_path[0];
+                    auto [ball_row_low, ball_row_high] = std::minmax(ball_prev_path_point.row, ball_path[j].row);
+                    auto [ball_col_low, ball_col_high] = std::minmax(ball_prev_path_point.col, ball_path[j].col);
+
+                    for (int k = 1; k < initial_path.size(); ++k) {
+                        auto [initial_row_low, initial_row_high] = std::minmax(initial_prev_path_point.row, initial_path[k].row);
+                        auto [initial_col_low, initial_col_high] = std::minmax(initial_prev_path_point.col, initial_path[k].col);
+
+//                      row_start_first <= row_end_second
+//                      && row_end_first >= row_start_second
+//                      && col_start_first <= col_end_second
+//                      && col_end_first >= col_start_second
+
+//                        std::cout << '(' << initial_row_low << ' ' << initial_col_low << ") (" << initial_row_high << ' ' << initial_col_high << ")\n";
+//                        std::cout << '(' << ball_row_low << ' ' << ball_col_low << ") (" << ball_row_high << ' ' << ball_col_high << ")" << std::endl;
 //
-//    for (const std::string &s: course) {
-//        std::cout << s << '\n';
-//    }
+                        if (initial_row_low <= ball_row_high
+                            && initial_row_high >= ball_row_low
+                            && initial_col_low <= ball_col_high
+                            && initial_col_high >= ball_col_low) {
+//                            std::cout << "invalid_path" << std::endl;
+                            valid_path = false;
+                            break;
+                        }
+
+                        initial_prev_path_point = initial_path[k];
+                    }
+                    ball_prev_path_point = ball_path[j];
+                }
+
+                if (valid_path) {
+                    valid_paths.emplace_back(ball_path);
+                }
+            }
+
+            if (valid_paths.empty()) {
+                std::cout << "valid_paths.empty()\n";
+                //This initial_path is a failure
+                break;
+            }
+            paths_copy.emplace_back(valid_paths);
+        }
+
+        if (paths_copy.size() + 1 < passed_paths.size()) {
+            std::cout << "paths_copy.size(): " << paths_copy.size() << " passed_paths: " << passed_paths.size()
+                      << std::endl;
+            //This means the initial path is a failure
+            continue;
+        }
+
+        bool success = find_unique_solution(
+                paths_copy,
+                return_value
+        );
+
+        if (success) {
+            return_value.emplace_back(std::vector<std::vector<BallPathPoint>>{initial_path});
+            return true;
+        }
+    }
+
+    return false;
 }
 
 bool find_path_for_next_ball(
